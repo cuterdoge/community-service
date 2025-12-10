e8d5ff36c89de5df38ada23e8cf81a2d485df427
cuterdoge <lauyangjean@gmail.com>
2025-12-10 21:22:24 +0800

    increase image limt

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const config = require('./config');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Increased limit for image uploads
 
 // Serve static files
 app.use(express.static('docs'));
